64a836295d3734b532e3a8a9d13056460e9584bc
Remigiusz <remik.jaruga@gmail.com>
2026-03-08 22:50:29 +0100

    Added array sorting code

---
+++ sort.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int tab[] = {3, 4, 1, 5, 2};
+    int n = 5;
+
+    for (int i=0; i<n-1; i++) {
+        for (int j=0; j<n-i-1; j++) {
+            if(tab[j] > tab[j+1]) {
+                int temp = tab[j];
+                tab[j] = tab[j+1];
+                tab[j+1] = temp; 
+
+            }
+        }
+    }
+
+    for(int i=0; i<5; i++) {
+        printf("%d ", tab[i]);
+    }
+
+    return 0;
+}
